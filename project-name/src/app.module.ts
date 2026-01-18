40ae0c4d877782de6b9eac71b573239226ae4a93
L0rdlung3r <mail.an.yanik@gmail.com>
2026-01-18 12:32:31 +0100

    created auth and user classes - TBC
    Signed-off-by: L0rdlung3r <mail.an.yanik@gmail.com>

--- project-name/src/app.module.ts
+++ project-name/src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -23,6 +25,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       }),
       inject: [ConfigService],
     }),
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
